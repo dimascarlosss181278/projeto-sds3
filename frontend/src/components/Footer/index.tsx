309d469354d5a4c048af7943051f9c1d4dcc193d
Dimas Santos <32331120+dimascarlosss181278@users.noreply.github.com>
2022-07-07 15:45:40 -0300

    Update index.tsx

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,9 +3,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/dimascarlosss181278000000" target="_blank" rel="noreferrer">Dimas Santos</a></p>
-                <p className="text-light"><small><strong>Semana Spring React Novembro 2021</strong><br />
-                    Uninassau - Universidade Mauricio de Nassau - DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/dimascarlosss181278" target="_blank" rel="noreferrer">Dimas Santos</a></p>
+                <p className="text-light"><small><strong>APP Desenvolvido durante a Semana Spring React</strong><br />
+                    DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
